1987196d700886ad7a16f20ff621ee379afc135b
HighImp <Seb.Koell@gmail.com>
2024-09-15 22:47:22 +0200

    wip

--- src/datalama.py
+++ src/datalama.py
@@ -12,7 +12,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-class DataLama:
+class DataLama: 
     def __init__(self,
                  persistent_dir: Path,
                  path_to_data: Path,
